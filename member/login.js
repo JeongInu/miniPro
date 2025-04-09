91cda65d2493850a7f977610880b219e05f911da
Rosy <rosy.jihye.noh@gmail.com>
2025-04-09 19:11:55 +0900

    memeber commit3

---
+++ member/login.js
@@ -0,0 +1,16 @@
+$(function(){
+    
+    $('#loginBtn').click(function(e){
+        e.preventDefault();
+        const id = $('#loginId').val();
+        const passWord = $('#loginPassword').val();
+        console.log(id, passWord);
+
+    })
+
+});
+
+
+
+
+
